e1ce1ba2b940b0a65d7ea978448146097af777d5
Luca Jungnickel <jungnickel.luca@gmail.com>
2022-05-30 17:37:54 +0200

    finished robot with calibration stone and init calibration sorting

--- NXT/MainRoutine/mainTask.h
+++ NXT/MainRoutine/mainTask.h
@@ -5,23 +5,6 @@
 #include "kernel_id.h"
 
 /* OSEK declarations */
-DeclareCounter(SysTimerCnt);
 
-DeclareTask(EventDispatcherTask);
-DeclareTask(EventBigButtonTask);
-DeclareTask(EventRightButtonTask);
-DeclareTask(LCDTask);
-DeclareTask(MainTask);
-
-DeclareEvent(BigButtonOnEvent); /* Event declaration */
-DeclareEvent(BigButtonOffEvent); /* Event declaration */
-DeclareEvent(RightButtonOnEvent);
-DeclareEvent(RightButtonOffEvent);
-
-DeclareResource(ResourceLightThreshold);
-DeclareResource(ResourceDirection);
-DeclareResource(ResourceBoxesRemaining);
-DeclareResource(ResourceNextBoxTarget);
-DeclareResource(ResourceCurrentBox);
 
 #endif //MAIN_TASK_H
