90eef398cb520066114d16bcbfb2e63081e7ec23
[Tristan Brech] <[tristan.brech98@gmail.com]>
2022-06-05 17:50:57 +0200

    Refactor into class and output of color and error message if more than one brick (of different colours)

--- colorDetection/colordetection.cpp
+++ colorDetection/colordetection.cpp
@@ -1,8 +1,7 @@
 //
-// Created by Timo Oeltze on 31.05.22.
+// Created by tristan on 05.06.22.
 //
-
-#include "colordetection.h"
+#include <colordetection.h>
 #include <iostream>
 #include <string.h>
 #include <opencv2/core.hpp>
@@ -13,11 +12,22 @@
 using namespace std;
 using namespace cv;
 
-/**
- * returns biggest area for parameter mask or null pointer
- * @param mask
- * @return null pointer or vector
- */
+vector<Point> ColorDetection::getRedCounter() {
+    return redCounter;
+}
+
+vector<Point> ColorDetection::getGreenCounter() {
+    return greenCounter;
+}
+
+vector<Point> ColorDetection::getBlueCounter() {
+    return blueCounter;
+}
+
+vector<Point> ColorDetection::getYellowCounter() {
+    return yellowCounter;
+}
+
 vector<Point> detect_max_color(Mat mask) {
     int max_area = 0;
     vector<vector<Point> > contours;
@@ -27,7 +37,7 @@ vector<Point> detect_max_color(Mat mask) {
 
     for(int i = 0; i < contours.size(); i++){
         int area = (int)cv::contourArea(contours[i]);
-        if (area > max_area) {
+        if (area > max_area && area > 4000) {
             large_contour = contours[i];
             max_area = area;
         }
@@ -43,7 +53,7 @@ void box_creator(Mat img, vector<Point> contour, int r, int g, int b, string nam
     putText(img, name, contour[0], FONT_ITALIC, 0.8, Scalar (139, 35, 35));
 }
 
-void show(Mat img, vector<Point> red, vector<Point> green, vector<Point> blue, vector<Point> yellow) {
+void show(Mat img, vector<Point> red, vector<Point> green, vector<Point> blue, vector<Point> yellow, ColorDetection newDetection) {
     if (red.size() > 0) {
         box_creator(img, red, 0, 0, 255, "Red Colour");
     }
@@ -56,38 +66,34 @@ void show(Mat img, vector<Point> red, vector<Point> green, vector<Point> blue, v
     if (yellow.size() > 0) {
         box_creator(img, yellow, 255, 255, 255, "Yellow Colour");
     }
+    namedWindow("TEST", WINDOW_NORMAL);
+    imshow("teST", newDetection.img);
+    waitKey(12000);
+    destroyAllWindows();
 }
 
-int main(int argc,char* argv[]) {
-    Mat img, img_hsv;
+ColorDetection::ColorDetection(char* filename) {
+    Mat img_hsv;
     Mat red_mask, green_mask, blue_mask, yellow_mask;
     Mat res_red, res_green, res_blue, res_yellow;
 
     // read picture
-    img = imread(argv[1]);
+    img = imread(filename);
 
     // convert from RGB to HSV
     cvtColor(img, img_hsv, COLOR_BGR2HSV);
 
-    // set color range red and define mask
-    Scalar red_lower = Scalar(136, 87, 111);
-    Scalar red_upper = Scalar(180, 255, 255);
+    //define red mask
     inRange(img_hsv, red_lower, red_upper, red_mask);
 
-    // set color range green and define mask
-    Scalar green_lower = Scalar(36, 25, 25);
-    Scalar green_upper = Scalar(76, 255, 255);
+    //define green mask
     inRange(img_hsv, green_lower, green_upper, green_mask);
 
-    // set color range blue and define mask
-    Scalar blue_lower = Scalar(94, 80, 2);
-    Scalar blue_upper = Scalar(114, 255, 255);
+    //define blue mask
     inRange(img_hsv, blue_lower, blue_upper, blue_mask);
 
-    // set color range yellow and define mask
-    Scalar yellow_lower = Scalar(15, 100, 100);
-    Scalar yellow_upper = Scalar(30, 255, 255);
-    inRange(img_hsv, yellow_lower, yellow_upper, yellow_mask);
+    //define yellow mask
+    inRange(img_hsv, yellow_lower,yellow_upper, yellow_mask);
 
     dilate(red_mask, red_mask, CV_8U);
     dilate(green_mask, green_mask, CV_8U);
@@ -99,16 +105,51 @@ int main(int argc,char* argv[]) {
     bitwise_and(img, img, res_blue, blue_mask);
     bitwise_and(img, img, res_yellow, yellow_mask);
 
+    redCounter = detect_max_color(red_mask);
+    greenCounter = detect_max_color(green_mask);
+    blueCounter = detect_max_color(blue_mask);
+    yellowCounter = detect_max_color(yellow_mask);
 
-    vector<Point> redCounter = detect_max_color(red_mask);
-    vector<Point> greenCounter = detect_max_color(green_mask);
-    vector<Point> blueCounter = detect_max_color(blue_mask);
-    vector<Point> yellowCounter = detect_max_color(yellow_mask);
-    show(img, redCounter, greenCounter, blueCounter, yellowCounter);
+    if(redCounter.size() > 0){
+        redBrick = true;
+    }else{
+        redBrick = false;
+    }
 
+    if(greenCounter.size() > 0){
+        greenBrick = true;
+    }else{
+        greenBrick = false;
+    }
 
-    namedWindow(argv[1], WINDOW_NORMAL);
-    imshow(argv[1], img);
-    waitKey(6000);
-    destroyAllWindows();
+    if(blueCounter.size() > 0){
+        blueBrick = true;
+    }else{
+        blueBrick = false;
+    }
+
+    if(yellowCounter.size() > 0){
+        yellowBrick = true;
+    }else{
+        yellowBrick = false;
+    }
+}
+
+int main(int argc, char* argv[]){
+    ColorDetection newDetection(argv[1]);
+    if(newDetection.redBrick != newDetection.greenBrick != newDetection.blueBrick != newDetection.yellowBrick){
+        if(newDetection.redBrick){
+            cout << "Red Brick On Line";
+        }else if(newDetection.greenBrick){
+            cout << "Green Brick On Line";
+        }else if(newDetection.blueBrick){
+            cout << "Blue Brick On Line";
+        }else if(newDetection.yellowBrick){
+            cout << "Yellow Brick On Line";
+        }
+    }else{
+        cout << "Mistake more than one brick";
+    }
+    cout << "\n";
+    //show(newDetection.img, newDetection.redCounter, newDetection.greenCounter, newDetection.blueCounter, newDetection.yellowCounter, newDetection);
 }
--- colorDetection/colordetection.h
+++ colorDetection/colordetection.h
@@ -4,10 +4,40 @@
 
 #ifndef EES_COLORDETECTION_H
 #define EES_COLORDETECTION_H
+#include <iostream>
+#include <string.h>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
 
+using namespace std;
+using namespace cv;
 
-class colordetection {
-
+class ColorDetection {
+public:
+        Mat img;
+        vector<Point> redCounter;
+        vector<Point> greenCounter;
+        vector<Point> blueCounter;
+        vector<Point> yellowCounter;
+        bool redBrick;
+        bool greenBrick;
+        bool blueBrick;
+        bool yellowBrick;
+        Scalar red_lower = Scalar(136, 87, 111);
+        Scalar red_upper = Scalar(180, 255, 255);
+        Scalar green_lower = Scalar(36, 25, 25);
+        Scalar green_upper = Scalar(76, 255, 255);
+        Scalar blue_lower = Scalar(94, 80, 2);
+        Scalar blue_upper = Scalar(114, 255, 255);
+        Scalar yellow_lower = Scalar(15, 100, 100);
+        Scalar yellow_upper = Scalar(30, 255, 255);
+        ColorDetection(char* filename);
+        vector<Point> getRedCounter();
+        vector<Point> getGreenCounter();
+        vector<Point> getBlueCounter();
+        vector<Point> getYellowCounter();
 };
 
 
