afcb13a17b2c7fd5bec329a9812592eca54d48bb
Timo Oeltze <timo.oeltze@icloud.com>
2022-06-13 15:19:30 +0200

    basic structure for controller

---
+++ Controller/controller.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Timo Oeltze on 13.06.22.
+//
+
+#include "controller.h"
---
+++ Controller/controller.h
@@ -0,0 +1,16 @@
+//
+// Created by Timo Oeltze on 13.06.22.
+//
+
+#ifndef EES_CONTROLLER_H
+#define EES_CONTROLLER_H
+
+
+
+class controller {
+
+};
+
+
+
+#endif //EES_CONTROLLER_H
