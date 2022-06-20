a2d06bafd6af5b886bbb3fd4246bf8970d923e9c
[Tristan Brech] <[tristan.brech98@gmail.com]>
2022-06-20 16:50:15 +0200

    2 Directories into one

---
+++ RASPI/Controller/controller.cpp
@@ -0,0 +1,52 @@
+#include "controller.h"
+#include "../colorDetection/colordetection.h"
+
+int init_ps() {
+    return 4;
+}
+
+bool hasConnection(int value) {
+    if (value > 0) {
+        return true;
+    }
+
+    return false;
+}
+
+bool readyForNewObject() {
+    //TODO: readyForNewObject from API
+    return true;
+}
+
+void mainRoutine(Controller newController) {
+    bool ready = readyForNewObject();
+
+    while (ready) {
+        //TODO: take picture
+        char* filepath = "../picturesFinalPosition/blueBrickOnLineLeft.jpg";
+        ColorDetection newColorDetection(filepath);
+
+        if (newColorDetection.onlyOneColor()) {
+
+        } else if (//){
+            //TODO: return error (255) - more then one brick (same color) on the line, no brick on the line
+        }
+
+        ready = readyForNewObject();
+    }
+
+}
+
+int main(int argc, char* argv[]){
+    Controller newController;
+    newController.buckets = init_ps();
+    newController.hasConnection = hasConnection(newController.buckets);
+
+    if (newController.hasConnection) {
+        mainRoutine(newController);
+    }
+}
+
+Controller::Controller() {
+    hasConnection = false;
+}
---
+++ RASPI/Controller/controller.h
@@ -0,0 +1,19 @@
+//
+// Created by Timo Oeltze on 13.06.22.
+//
+
+#ifndef EES_CONTROLLER_H
+#define EES_CONTROLLER_H
+
+using namespace std;
+//enum Color {red, blue, yellow, green};
+
+class Controller {
+public:
+    int buckets;
+    bool hasConnection;
+    Controller();
+};
+
+
+#endif //EES_CONTROLLER_H
