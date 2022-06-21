75fef06a41ab21f11911d064568c82239f0fbb09
filiprejmus <anarchiaonline@gmail.com>
2022-06-21 14:57:01 +0200

    More refactoring

--- RASPI/BluetoothService.cpp
+++ RASPI/BluetoothService.cpp
@@ -12,12 +12,39 @@
 #define PREDICTED_BUCKET 4
 
 
-
-	
+int BluetoothService::InitRoutine()
+{
+	int status;
+	int msg_type;
+	int buckets = -1;
+	if (s != -1)
+	{
+		uint8_t message_type = INIT;
+		uint8_t payload = 0;
+		status = send_msg(s, send_buf, recv_buf, message_type, payload);
+		msg_type = receive_msg(s, recv_buf, send_buf);
+		if (msg_type != INIT)
+		{
+			printf("Protocol Breach, expected INIT command\n");
+		}
+		else
+		{
+			buckets = recv_buf[1];
+			printf("We got %d buckets \n", buckets);
+			message_type = ACK;
+			payload = 0;
+			status = send_msg(s, send_buf, recv_buf, message_type, payload);
+		}
+	}
+	if(buckets < 0){
+		printf("Amount of buckets wrong...\n");
+	}
+	return buckets;
+}
 
 int BluetoothService::unmarshal(uint8_t *recv_buf, int bytes_read)
 {
-		if (bytes_read != 4)
+	if (bytes_read != 4)
 	{
 		printf("Wrong amount of bytes received! %d Bytes read! \n", bytes_read);
 		return -1;
@@ -40,26 +67,7 @@ int BluetoothService::unmarshal(uint8_t *recv_buf, int bytes_read)
 		printf("Checksum Error! \n");
 		return -1;
 	}
-	return recv_buf[0];
-}
-
-void BluetoothService::marshal(uint8_t *send_buf, uint8_t message_type, uint8_t payload)
-{
-	send_buf[0] = message_type;
-	send_buf[1] = payload;
-	send_buf[2] = ~send_buf[0];
-	send_buf[3] = ~send_buf[1];
-}
-
-int BluetoothService::receive_msg(int socket, uint8_t *recv_buf)
-{
-	int bytes_read = 0;
-	while (bytes_read == 0)
-	{
-		bytes_read = read(socket, recv_buf, sizeof(recv_buf));
-		sleep(1);
-	}
-	int msg_type = unmarshal(recv_buf, bytes_read);
+	int msg_type = recv_buf[0];
 	switch (msg_type)
 	{
 	case ACK:
@@ -84,11 +92,61 @@ int BluetoothService::receive_msg(int socket, uint8_t *recv_buf)
 	}
 	return msg_type;
 }
-int BluetoothService::send_msg(int socket, uint8_t *send_buf, uint8_t message_type, uint8_t payload)
+
+
+void BluetoothService::marshal(uint8_t *send_buf, uint8_t message_type, uint8_t payload)
+{
+	send_buf[0] = message_type;
+	send_buf[1] = payload;
+	send_buf[2] = ~send_buf[0];
+	send_buf[3] = ~send_buf[1];
+}
+
+int BluetoothService::receive_msg(int socket, uint8_t* recv_buf, uint8_t* send_buf)
+{
+	int bytes_read = 0;
+	int status =0;
+	while (bytes_read == 0)
+	{
+		bytes_read = read(socket, recv_buf, sizeof(recv_buf));
+		sleep(1);
+	}
+	int msg_type = unmarshal(recv_buf, bytes_read);
+	if(msg_type == -1){
+		marshal(send_buf, MSG_RESEND, 0);
+	}
+	else{
+		marshal(send_buf, ACK, 0);
+	}
+	
+	status = write(socket, send_buf, 4);
+	if(status == 0){
+		printf("ACK sent!\n");
+	}
+	return msg_type;
+}
+int BluetoothService::send_msg(int socket, uint8_t* send_buf, uint8_t* recv_buf, uint8_t message_type, uint8_t payload)
 {
 	marshal(send_buf, message_type, payload);
 	int status = write(socket, send_buf, 4);
-	return status;
+	int bytes_read = 0;
+	while (bytes_read == 0)
+	{
+		bytes_read = read(socket, recv_buf, sizeof(recv_buf));
+		sleep(1);
+	}
+	int msg_type = unmarshal(recv_buf, bytes_read);
+	if(msg_type==ACK){
+		return status;		
+	}
+	else if(msg_type==MSG_RESEND){
+		send_msg(socket, send_buf, recv_buf, message_type, payload);
+		return status;
+	}
+	else{
+		printf("Unexpected Message type received, expected ack.\n");
+		return -1;
+	}
 }
 
 BluetoothService::BluetoothService()
@@ -117,6 +175,7 @@ BluetoothService::BluetoothService()
 	else
 	{
 		perror("Oh no;");
+		s = -1;
 
 	}
 }
@@ -130,36 +189,8 @@ int main(int argc, char **argv)
 {
 
 	BluetoothService bt;
-	/* int status;
-	int msg_type;
-	if (s != -1)
-	{
-		uint8_t message_type = INIT;
-		uint8_t payload = 0;
-		status = send_msg(s, send_buf, message_type, payload);
-		printf("Sent!\n");
-		msg_type = receive_msg(s, recv_buf);
-		if (msg_type != ACK)
-		{
-			printf("Expected ACK but got something else back\n");
-		}
-		msg_type = receive_msg(s, recv_buf_2);
-		if (msg_type != INIT)
-		{
-			printf("Protocol Breach, expected INIT command\n");
-		}
-		else
-		{
-			printf("We got %d buckets \n", recv_buf_2[1]);
-			message_type = ACK;
-			payload = 0;
-			status = send_msg(s, send_buf, message_type, payload);
-		}
-	}
-
-	// read data from the client
-	*/
-
+	int buckets = bt.InitRoutine();
+	printf("%d Buckets in Main", buckets);
 	bt.CloseConnection();
 
 	return 0;
--- RASPI/BluetoothService.h
+++ RASPI/BluetoothService.h
@@ -11,9 +11,10 @@ public:
     BluetoothService();
     int unmarshal(uint8_t* recv_buf, int bytes_read);
     void marshal(uint8_t* send_buf, uint8_t message_type, uint8_t payload);
-    int receive_msg(int socket, uint8_t* recv_buf);
-    int send_msg(int socket, uint8_t* send_buf, uint8_t message_type, uint8_t payload);
+    int receive_msg(int socket, uint8_t* recv_buf, uint8_t* send_buf);
+    int send_msg(int socket, uint8_t* send_buf, uint8_t* recv_buf, uint8_t message_type, uint8_t payload);
     void CloseConnection();
+    int InitRoutine();
 };
 
 
