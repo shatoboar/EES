8267d003a2ce58d238bd2c655a1c128e8416ef41
filiprejmus <anarchiaonline@gmail.com>
2022-06-28 11:50:53 +0200

    Making funcs private and wip for CMakeLists

--- RASPI/BluetoothService/BluetoothService.h
+++ RASPI/BluetoothService/BluetoothService.h
@@ -9,12 +9,12 @@ private:
     uint8_t recv_buf[4];
 	uint8_t send_buf[4];
     int s;
-public:
-    BluetoothService();
     int unmarshal(uint8_t* recv_buf, int bytes_read);
     void marshal(uint8_t* send_buf, uint8_t message_type, uint8_t payload);
     int receive_msg(int socket, uint8_t* recv_buf, uint8_t* send_buf);
     int send_msg(int socket, uint8_t* send_buf, uint8_t* recv_buf, uint8_t message_type, uint8_t payload);
+public:
+    BluetoothService();
     void CloseConnection();
     int InitRoutine();
     void DeployRoutine();
